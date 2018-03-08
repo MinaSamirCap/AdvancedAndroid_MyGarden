3a9c4c907964b830d6130fcc2343c26b0650d136
MinaSamirCap <MenaSamerCap@gmail.com>
2018-03-08 12:39:47 +0200

    sovle quiz

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -37,8 +37,15 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
-        // TODO (4): Create a PendingIntent for the PlantWateringService and setOnClickPendingIntent for widget_water_button
+        // done (4): Create a PendingIntent for the PlantWateringService and setOnClickPendingIntent for widget_water_button
         // Instruct the widget manager to update the widget
+
+        Intent intentWater = new Intent(context, PlantWateringService.class);
+        intentWater.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent pendingIntentWater = PendingIntent.getService(context, 0, intentWater,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.widget_water_button, pendingIntent);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
